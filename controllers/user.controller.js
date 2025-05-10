4f2955550f99d66d0ae144c6e28711a359629b98
Tran Hoang Nhat <kundeeptryvl@gmail.com>
2025-05-10 10:32:01 +0700

    Implements load snaps by userId

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -202,3 +202,40 @@ module.exports.checkUsernameExist = async (req, res) => {
     return apiResponse(res, 400, 'Failed to check exist email.');
   }
 }
+
+// [POST]: BASE_URL/api/users/check-password
+module.exports.checkPassword = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { password } = req.body;
+
+    if (!password) {
+      return apiResponse(res, 400, 'Password is required.');
+    }
+
+    const user = await User.findById(userId).select('password');
+
+    if (!user) {
+      return apiResponse(res, 404, 'User not found.');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      return res.json({
+        code: 200,
+        isValid: true,
+        message: 'Password is correct.'
+      });
+    } else {
+      return res.json({
+        code: 200,
+        isValid: false,
+        message: 'Password is incorrect.'
+      });
+    }
+  } catch (e) {
+    console.error('Check password error:', e);
+    return apiResponse(res, 400, 'Failed to check password.');
+  }
+};
